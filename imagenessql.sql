699f49267b77bab43ccf20b65a378b5c17dd59f8
alphyon <alphyon21@yahoo.es>
2014-05-21 22:43:09 -0600

    full

--- imagenessql.sql
+++ imagenessql.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-05-2014 a las 06:35:38
+-- Tiempo de generación: 22-05-2014 a las 06:39:15
 -- Versión del servidor: 5.6.15-log
 -- Versión de PHP: 5.4.24
 
@@ -19,7 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `imagenes`
 --
-
+CREATE DATABASE IF NOT EXISTS `imagenes`;
+USE `imagenes`;
 -- --------------------------------------------------------
 
 --
@@ -31,7 +32,14 @@ CREATE TABLE IF NOT EXISTS `imgarchivo` (
   `ruta` varchar(250) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Volcado de datos para la tabla `imgarchivo`
+--
+
+INSERT INTO `imgarchivo` (`id`, `ruta`, `nombre`) VALUES
+(1, '/img/0b8322dafc02db75c7e18b338f5c3a21_20822000.png', '0b8322dafc02db75c7e18b338f5c3a21_20822000');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
